e898be8756983ae1b9d4d5945aad4359ae38c404
pablopunk <pablovarela182@gmail.com>
2019-06-08 17:47:16 +0200

    Simpler

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict';
 const graphqlGot = require('graphql-got');
 const controlAccess = require('control-access');
-const etag = require('etag');
+require('dotenv').config()
 
 const token = process.env.GITHUB_TOKEN;
 const username = process.env.GITHUB_USERNAME;
-const origin = process.env.ACCESS_ALLOW_ORIGIN;
 const cache = `max-age=${Number(process.env.CACHE_MAX_AGE) || 300}`;
 const maxRepos = Number(process.env.MAX_REPOS) || 6;
-const ONE_DAY = 1000 * 60 * 60 * 24;
 
 if (!token) {
 	throw new Error('Please set your GitHub token in the `GITHUB_TOKEN` environment variable');
@@ -17,11 +15,6 @@ if (!token) {
 if (!username) {
 	throw new Error('Please set your GitHub username in the `GITHUB_USERNAME` environment variable');
 }
-
-if (!origin) {
-	throw new Error('Please set the `access-control-allow-origin` you want in the `ACCESS_ALLOW_ORIGIN` environment variable');
-}
-
 const query = `
 	query {
 		user(login: "${username}") {
@@ -56,9 +49,6 @@ const query = `
 	}
 `;
 
-let responseText = '[]';
-let responseETag = '';
-
 async function fetchRepos() {
 	const {body} = await graphqlGot('api.github.com/graphql', {
 		query,
@@ -70,23 +60,12 @@ async function fetchRepos() {
 		stargazers: repo.stargazers.totalCount,
 		forks: repo.forks.totalCount
 	}));
-	responseText = JSON.stringify(repos);
-	responseETag = etag(responseText);
+	return JSON.stringify(repos);
 }
 
-setInterval(fetchRepos, ONE_DAY);
-fetchRepos();
+module.exports = async (request, response) => {
+  const responseText = await fetchRepos()
 
-module.exports = (request, response) => {
-	controlAccess()(request, response);
 	response.setHeader('cache-control', cache);
-	response.setHeader('etag', responseETag);
-
-	if (request.headers.etag === responseETag) {
-		response.statusCode = 304;
-		response.end();
-		return;
-	}
-
 	response.end(responseText);
 };
